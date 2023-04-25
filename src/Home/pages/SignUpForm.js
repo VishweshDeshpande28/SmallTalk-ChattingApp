7b52d787e53424cf7cfef7f10c4e8e8eb971a8ce
Vishwesh Deshpande <vishwesh.deshpande28@gmail.com>
2023-04-25 05:50:01 +0530

    new changes

--- src/Home/pages/SignUpForm.js
+++ src/Home/pages/SignUpForm.js
@@ -1,38 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 
-class SignUpForm extends Component {
-  constructor() {
-    super();
+function SignUpForm() {
 
-    this.state = {
-      name: "",
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    let target = event.target;
-    let value = target.type === "checkbox" ? target.checked : target.value;
-    let name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-
-    console.log("The form was submitted with the following data:");
-    console.log(this.state);
-  }
-
-  render() {
     return (
       <div className="formCenter">
-        <form onSubmit={this.handleSubmit} className="formFields">
+        <form className="formFields">
           <div className="formField">
       <h1 className="app-heading">Welcome To SmallTalk!</h1>
             <label className="formFieldLabel" htmlFor="name">
@@ -44,13 +16,11 @@ class SignUpForm extends Component {
               className="formFieldInput"
               placeholder="Enter your name"
               name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
             />
           </div>
         </form>
       </div>
     );
   }
-}
+
 export default SignUpForm;
